dc68383988b46ccc036671f5b43b53d6ff51a779
J-Dog <j-dog@j-dog.net>
2026-06-11 17:28:10 -0700

    fix(utxo-tracker): Litecoin dust threshold is 5460 litoshis, not 546

--- src/CryptoNetworks.js
+++ src/CryptoNetworks.js
@@ -82,7 +82,7 @@ class CryptoNetworks {
                     "pubKeyHash": 0x30,
                     "scriptHash": 0x32,
                     "wif": 0xb0,
-                    "dustThreshold": 546
+                    "dustThreshold": 5460
                 }
             case "litecoin-testnet":
                 return {
@@ -95,7 +95,7 @@ class CryptoNetworks {
                     "pubKeyHash": 0x6f,
                     "scriptHash": 0xc4,
                     "wif": 0xef,
-                    "dustThreshold": 546
+                    "dustThreshold": 5460
                 }
             case "litecoin-regtest":
                 return {
@@ -108,7 +108,7 @@ class CryptoNetworks {
                     "pubKeyHash": 0x6f,
                     "scriptHash": 0xc4,
                     "wif": 0xef,
-                    "dustThreshold": 546
+                    "dustThreshold": 5460
                 }   
         }
     }
